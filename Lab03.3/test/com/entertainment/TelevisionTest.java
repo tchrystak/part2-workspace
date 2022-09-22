2c3a97722cec8f2641efc5ac163c66a1aeb0051a
tchrystak <chrystaktrey@gmail.com>
2022-09-22 08:22:10 -0700

    Working on lab 3.3 continued...

--- Lab03.3/test/com/entertainment/TelevisionTest.java
+++ Lab03.3/test/com/entertainment/TelevisionTest.java
@@ -6,13 +6,13 @@ import org.junit.Test;
 
 public class TelevisionTest {
 
+  private final Television tv1 = new Television("Sony", 50, DisplayType.LED);
+
   @Test
   public void testSetVolume() {
-    Television tv = new Television();
-    tv.setVolume(100);
-    assertEquals(100, tv.getVolume());
-    tv.setVolume(0);
-    assertEquals(0, tv.getVolume());
+    int volume = (Television.MIN_VOLUME + Television.MAX_VOLUME) / 2;
+    tv1.setVolume(volume);
+    assertEquals(volume, tv1.getVolume());
   }
 
 //  @Test(expected = IllegalArgumentException.class)
@@ -21,29 +21,34 @@ public class TelevisionTest {
 //    tv.setVolume(110);
 //  }
 
-  @Test
-  public void testSetVolumeException() {
-    try {
-      Television tv = new Television();
-      tv.setVolume(110);
-      fail("Should have thrown IllegalArgumentException");
-    } catch (IllegalArgumentException e) {
-      assertEquals("Invalid volume: 110. Allowed range: [0, 100].", e.getMessage());
+  @Test (expected = IllegalArgumentException.class)
+  public void testSetVolumeIllegalArgumentException() {
+      tv1.setVolume(Television.MIN_VOLUME - 1);
     }
-  }
+//    try {
+//      Television tv = new Television();
+//      tv.setVolume(110);
+//      fail("Should have thrown IllegalArgumentException");
+//    } catch (IllegalArgumentException e) {
+//      assertEquals("Invalid volume: 110. Allowed range: [0, 100].", e.getMessage());
+//    }
+// }
 
   @Test
     public void testChangeChannel () throws InvalidChannelException {
-      Television tv = new Television();
-      tv.changeChannel(12);
-      assertEquals(12, tv.getCurrentChannel());
+      int channel = (Television.MIN_CHANNEL + Television.MAX_CHANNEL) / 2;
+    tv1.changeChannel(channel);
+    assertEquals(channel, tv1.getCurrentChannel());
+//      Television tv = new Television();
+//      tv.changeChannel(12);
+//      assertEquals(12, tv.getCurrentChannel());
     }
 
   @Test
   public void testChangeChannelException() throws InvalidChannelException {
     try {
-      Television tv = new Television();
-      tv.changeChannel(0);
+      Television tv1 = new Television();
+      tv1.changeChannel(0);
       fail("Should have thrown InvalidChannelException");
     } catch (InvalidChannelException e) {
       assertEquals("Invalid channel: 0. Allowed range: [1,999].", e.getMessage());
@@ -52,8 +57,8 @@ public class TelevisionTest {
 
   @Test
   public void TestCompareTo() {
-    Television tv2 = new Television("Sony");
-    assertTrue(tv1.compareto(tv2) < 0);
+    Television tv2 = new Television("Vizio");
+    assertTrue(tv1.compareTo(tv2) < 0);
     Television tv3 = new Television("LG");
     assertTrue(tv1.compareTo(tv3) > 0);
   }
@@ -63,10 +68,14 @@ public class TelevisionTest {
     Television tv2 = new Television("Samsung", 0, DisplayType.OLED);
     assertEquals(tv1.hashCode(), tv2.hashCode());
     Television tv3 = new Television("Samsung", 1, DisplayType.OLED);
-    assertEquals(tv.1.hashCode(), tv3.hashCode());
+    assertEquals(tv1.hashCode(), tv3.hashCode());
   }
 
   @Test
   public void testEquals() {
+    Television tv2 = new Television("Samsung", 0, DisplayType.OLED);
+    assertEquals(tv1, tv2);
+    Television tv3 = new Television("Samsung", 0, DisplayType.LCD);
+    assertNotEquals(tv1, tv3);
   }
 }
